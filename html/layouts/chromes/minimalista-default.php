48edc67f5876d68ed963bb9ce79ce76287e56268
Uziel <uziel.oliveira@intercode.com.br>
2023-02-22 12:04:10 -0300

    Ajustes de módulos

---
+++ html/layouts/chromes/minimalista-default.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  Templates.minimalista
+ *
+ * @copyright   (C) 2020 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\Utilities\ArrayHelper;
+
+$module = $displayData['module'];
+$params = $displayData['params'];
+$attribs = $displayData['attribs'];
+
+if ($module->content === null || $module->content === '') {
+    return;
+}
+
+$moduleTag     = $params->get('module_tag', 'div');
+$bootstrapSize = (int) $params->get('bootstrap_size', 0);
+$moduleClass   = $bootstrapSize !== 0 ? ' col-' . $params->get('default-bootstrap-desktop') : '';
+$headerTag     = $params->get('header_tag', 'h3');
+$headerClass   = $params->get('header_class', 'module-title');
+$moduleClassSfx = $params->get('moduleclass_sfx');
+// check if have space in the start of the string
+if ($moduleClassSfx){
+    if (substr($moduleClassSfx, 0, 1) !== ' ') {
+        $moduleClassSfx = ' ' . $moduleClassSfx;
+    }
+}
+
+$moduleAttribs = [];
+$moduleAttribs['class'] = 'module-default' . ' module-position-' . $module->position . ' module-id-' . $module->id . $moduleClass . $moduleClassSfx;
+$headerAttribs['class'] = 'module-title ' . $headerClass;
+
+
+
+
+
+// Only add aria if the moduleTag is not a div
+if ($moduleTag !== 'div') {
+    if ($module->showtitle) :
+        $moduleAttribs['aria-labelledby'] = 'module-' . $module->id;
+        $headerAttribs['id']              = 'module-' . $module->id;
+    else :
+        $moduleAttribs['aria-label'] = $module->title;
+    endif;
+}
+
+$header = '<' . $headerTag . ' ' . ArrayHelper::toString($headerAttribs) . '>' . $module->title . '</' . $headerTag . '>';
+
+?>
+
+
+<<?php echo $moduleTag; ?> <?php echo ArrayHelper::toString($moduleAttribs); ?>>
+   <?php if ($module->showtitle): ?>
+    <?php echo $header; ?>
+<?php endif; ?>
+   <?php if ($module->content): ?>
+    <div class="module-content">
+        <?php echo $module->content; ?>
+    </div>
+<?php endif; ?>
+</<?php echo $moduleTag; ?>>
+
+
--- index.php
+++ index.php
@@ -16,38 +16,59 @@ include_once JPATH_THEMES . '/' . $this->template . '/logic.php';
         <div class="container<?php echo $containerFluid; ?>">
             <div class="row">
                 <div class="col-12">
-                    <jdoc:include type="modules" name="header" style="none" />
+                    <jdoc:include type="modules" name="header" style="<?php echo $this->template.'-default';?>" />
                 </div>
             </div>
         </div>
         <div class="container<?php echo $containerFluid; ?>">
             <div class="row">
                 <div class="col-12">
-                    <jdoc:include type="modules" name="menu" style="none" />
+                    <jdoc:include type="modules" name="menu" style="<?php echo $this->template.'-default';?>" />
                 </div>
             </div>
         </div>
     </header>
     <main class="main">
         <div class="container<?php echo $containerFluid; ?>">
+        <?php if ($this->countModules('main-top')): ?>
+                <div class="main-top col-12">
+                    <jdoc:include type="modules" name="main-top" style="<?php echo $this->template . '-default'; ?>" />
+                </div>
+                <?php endif; ?>
             <div class="row">
+                
                <?php if ($this->countModules('sidebar-left')) : ?>
                 <div class="sidebar-left col-12 col<?php echo $defaultBoostrapDesktop; ?><?php echo $sidebarWidth; ?>">
-                    <jdoc:include type="modules" name="sidebar-left" style="none" />
+                    <jdoc:include type="modules" name="sidebar-left" style="<?php echo $this->template.'-default';?>" />
                 </div>
                 <?php endif; ?>
                 <div class="component col-12 col<?php echo $defaultBoostrapDesktop; ?>">
-                    <jdoc:include type="modules" name="main-top" style="none" />
+                    <jdoc:include type="modules" name="content-top" style="<?php echo $this->template.'-default';?>" />
                     <jdoc:include type="message" />
                     <jdoc:include type="component" />
-                    <jdoc:include type="modules" name="main-bottom" style="none" />
+                    <jdoc:include type="modules" name="content-bottom" style="<?php echo $this->template.'-default';?>" />
                 </div>
                 <?php if ($this->countModules('sidebar-right')) : ?>
                 <div class="sidebar-right col-12 col<?php echo $defaultBoostrapDesktop; ?><?php echo $sidebarWidth; ?>">
-                    <jdoc:include type="modules" name="sidebar-right" style="none" />
+                    <jdoc:include type="modules" name="sidebar-right" style="<?php echo $this->template.'-default';?>" />
+                </div>
+                <?php endif; ?>
+                <?php if ($this->countModules('main-bottom')) : ?>
+                <div class="main-bottom col-12">
+                    <jdoc:include type="modules" name="main-bottom" style="<?php echo $this->template.'-default';?>" />
                 </div>
                 <?php endif; ?>
             </div>
         </div>
+    </main>
+    <footer class="footer">
+        <div class="container<?php echo $containerFluid; ?>">
+            <div class="row">
+                <div class="col-12">
+                    <jdoc:include type="modules" name="footer" style="<?php echo $this->template.'-default';?>" />
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
