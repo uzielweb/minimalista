4c9d08332dc59fe392631d410de249f3569144ca
Uziel Almeida Oliveira <uzielweb@gmail.com>
2023-02-23 18:05:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -15,60 +15,66 @@ include_once JPATH_THEMES . '/' . $this->template . '/logic.php';
     <header class="header">
         <div class="container<?php echo $containerFluid; ?>">
             <div class="row">
-                <div class="col-12">
-                    <jdoc:include type="modules" name="header" style="<?php echo $this->template.'-default';?>" />
-                </div>
+                <jdoc:include type="modules" name="header" style="<?php echo $this->template.'-default';?>" />
             </div>
         </div>
         <div class="container<?php echo $containerFluid; ?>">
             <div class="row">
-                <div class="col-12">
-                    <jdoc:include type="modules" name="menu" style="<?php echo $this->template.'-default';?>" />
-                </div>
+                <jdoc:include type="modules" name="menu" style="<?php echo $this->template.'-default';?>" />
             </div>
         </div>
     </header>
     <main class="main">
         <div class="container<?php echo $containerFluid; ?>">
-        <?php if ($this->countModules('main-top')): ?>
-                <div class="main-top row">
-                    <jdoc:include type="modules" name="main-top" style="<?php echo $this->template . '-default'; ?>" />
-                </div>
-                <?php endif; ?>
+            <?php if ($this->countModules('main-top')): ?>
+            <div class="main-top row">
+                <jdoc:include type="modules" name="main-top" style="<?php echo $this->template . '-default'; ?>" />
+            </div>
+            <?php endif; ?>
             <div class="row">
-                
-               <?php if ($this->countModules('sidebar-left')) : ?>
+                <?php if ($this->countModules('sidebar-left')) : ?>
                 <div class="sidebar-left col-12 col<?php echo $defaultBoostrapDesktop; ?><?php echo $sidebarWidth; ?>">
-                    <jdoc:include type="modules" name="sidebar-left" style="<?php echo $this->template.'-default';?>" />
+                    <div class="row">
+                        <jdoc:include type="modules" name="sidebar-left" style="<?php echo $this->template.'-default';?>" />
+                    </div>
                 </div>
                 <?php endif; ?>
                 <div class="component col-12 col<?php echo $defaultBoostrapDesktop; ?>">
-                    <jdoc:include type="modules" name="content-top" style="<?php echo $this->template.'-default';?>" />
+                    <?php if ($this->countModules('content-top')) : ?>
+                    <div class="row">
+                        <jdoc:include type="modules" name="content-top" style="<?php echo $this->template.'-default';?>" />
+                    </div>
+                    <?php endif; ?>
                     <jdoc:include type="message" />
                     <jdoc:include type="component" />
-                    <jdoc:include type="modules" name="content-bottom" style="<?php echo $this->template.'-default';?>" />
+                    <?php if ($this->countModules('content-bottom')) : ?>
+                    <div class="row">
+                        <jdoc:include type="modules" name="content-bottom" style="<?php echo $this->template.'-default';?>" />
+                    </div>
+                    <?php endif;?>
                 </div>
                 <?php if ($this->countModules('sidebar-right')) : ?>
                 <div class="sidebar-right col-12 col<?php echo $defaultBoostrapDesktop; ?><?php echo $sidebarWidth; ?>">
-                    <jdoc:include type="modules" name="sidebar-right" style="<?php echo $this->template.'-default';?>" />
+                    <div class="row">
+                        <jdoc:include type="modules" name="sidebar-right" style="<?php echo $this->template.'-default';?>" />
+                    </div>
                 </div>
                 <?php endif; ?>
-               
             </div>
-             <?php if ($this->countModules('main-bottom')): ?>
-                <div class="main-bottom row">
-                    <jdoc:include type="modules" name="main-bottom" style="<?php echo $this->template . '-default'; ?>" />
-                </div>
-                <?php endif; ?>
+            <?php if ($this->countModules('main-bottom')): ?>
+            <div class="main-bottom row">
+                <jdoc:include type="modules" name="main-bottom" style="<?php echo $this->template . '-default'; ?>" />
+            </div>
+            <?php endif; ?>
         </div>
     </main>
     <footer class="footer">
         <div class="container<?php echo $containerFluid; ?>">
+            <?php if ($this->countModules('footer')): ?>
             <div class="row">
-                <div class="col-12">
-                    <jdoc:include type="modules" name="footer" style="<?php echo $this->template.'-default';?>" />
-                </div>
+                <jdoc:include type="modules" name="footer" style="<?php echo $this->template.'-default';?>" />
             </div>
+            <?php endif;?>
         </div>
     </footer>
 </body>
