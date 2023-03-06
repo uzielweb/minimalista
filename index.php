b0a1f80f63cc7bf736a38d5addd349719c67910e
Uziel <uziel.oliveira@intercode.com.br>
2023-03-06 17:07:41 -0300

    Ajuste

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ defined('_JEXEC') or die;
 include_once JPATH_THEMES . '/' . $this->template . '/logic.php';
 ?>
 <!doctype html>
-<html lang="<?php echo $doc->getLanguage(); ?>" dir="<?php echo $doc->getDirection(); ?>">
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <jdoc:include type="metas" />
