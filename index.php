b39f3c5d5d2fb0d0806a1258d462e106fa98645e
Uziel Almeida Oliveira <uzielweb@gmail.com>
2023-09-19 19:19:59 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,6 @@ include_once JPATH_THEMES . '/' . $this->template . '/logic.php';
 <!doctype html>
 <html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
-
     <jdoc:include type="metas" />
     <jdoc:include type="styles" />
     <jdoc:include type="scripts" />
@@ -15,27 +14,34 @@ include_once JPATH_THEMES . '/' . $this->template . '/logic.php';
     <header class="header">
         <div class="container<?php echo $containerFluid; ?>">
             <div class="row">
-                <jdoc:include type="modules" name="header" style="<?php echo $this->template.'-default';?>" />
+                <jdoc:include type="modules" name="header" style="<?php echo $this->template . '-default'; ?>" />
             </div>
         </div>
         <div class="container<?php echo $containerFluid; ?>">
-        <!-- navbar offcanvas bootstrap 5 -->
+            <!-- navbar offcanvas bootstrap 5 -->
             <nav class="navbar navbar-expand<?php echo $defaultBoostrapDesktop; ?>">
                 <div class="container-fluid">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                    <?php if ($logo): ?>
+                    <a class="navbar-brand" href="<?php echo $this->baseurl; ?>">
+                        <img src="<?php echo $logo; ?>" alt="<?php echo $logo_alt; ?>" />
+                    </a>
+                    <?php endif;?>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+                        data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                   
-                    <div class="offcanvas offcanvas-<?php echo $offcanvasDirection;?>" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-                        <div class="offcanvas-header">                        
-                            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    <div class="offcanvas offcanvas-<?php echo $offcanvasDirection; ?>" tabindex="-1"
+                        id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                        <div class="offcanvas-header">
+                            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                                aria-label="Close"></button>
                         </div>
                         <div class="offcanvas-body">
-                            <jdoc:include type="modules" name="menu" style="<?php echo $this->template.'-default';?>" />
+                            <jdoc:include type="modules" name="menu" style="<?php echo $this->template . '-default'; ?>" />
                         </div>
                     </div>
                 </div>
-           </nav>
+            </nav>
         </div>
     </header>
     <main class="main">
@@ -44,49 +50,53 @@ include_once JPATH_THEMES . '/' . $this->template . '/logic.php';
             <div class="main-top row">
                 <jdoc:include type="modules" name="main-top" style="<?php echo $this->template . '-default'; ?>" />
             </div>
-            <?php endif; ?>
+            <?php endif;?>
             <div class="row">
-                <?php if ($this->countModules('sidebar-left')) : ?>
+                <?php if ($this->countModules('sidebar-left')): ?>
                 <div class="sidebar-left col-12 col<?php echo $sidebarWidth; ?>">
                     <div class="row">
-                        <jdoc:include type="modules" name="sidebar-left" style="<?php echo $this->template.'-default';?>" />
+                        <jdoc:include type="modules" name="sidebar-left"
+                            style="<?php echo $this->template . '-default'; ?>" />
                     </div>
                 </div>
-                <?php endif; ?>
+                <?php endif;?>
                 <div class="component col-12 col<?php echo $defaultBoostrapDesktop; ?>">
-                    <?php if ($this->countModules('content-top')) : ?>
+                    <?php if ($this->countModules('content-top')): ?>
                     <div class="row">
-                        <jdoc:include type="modules" name="content-top" style="<?php echo $this->template.'-default';?>" />
+                        <jdoc:include type="modules" name="content-top"
+                            style="<?php echo $this->template . '-default'; ?>" />
                     </div>
-                    <?php endif; ?>
+                    <?php endif;?>
                     <jdoc:include type="message" />
                     <jdoc:include type="component" />
-                    <?php if ($this->countModules('content-bottom')) : ?>
+                    <?php if ($this->countModules('content-bottom')): ?>
                     <div class="row">
-                        <jdoc:include type="modules" name="content-bottom" style="<?php echo $this->template.'-default';?>" />
+                        <jdoc:include type="modules" name="content-bottom"
+                            style="<?php echo $this->template . '-default'; ?>" />
                     </div>
                     <?php endif;?>
                 </div>
-                <?php if ($this->countModules('sidebar-right')) : ?>
+                <?php if ($this->countModules('sidebar-right')): ?>
                 <div class="sidebar-right col-12 col<?php echo $sidebarWidth; ?>">
                     <div class="row">
-                        <jdoc:include type="modules" name="sidebar-right" style="<?php echo $this->template.'-default';?>" />
+                        <jdoc:include type="modules" name="sidebar-right"
+                            style="<?php echo $this->template . '-default'; ?>" />
                     </div>
                 </div>
-                <?php endif; ?>
+                <?php endif;?>
             </div>
             <?php if ($this->countModules('main-bottom')): ?>
             <div class="main-bottom row">
                 <jdoc:include type="modules" name="main-bottom" style="<?php echo $this->template . '-default'; ?>" />
             </div>
-            <?php endif; ?>
+            <?php endif;?>
         </div>
     </main>
     <footer class="footer">
         <div class="container<?php echo $containerFluid; ?>">
             <?php if ($this->countModules('footer')): ?>
             <div class="row">
-                <jdoc:include type="modules" name="footer" style="<?php echo $this->template.'-default';?>" />
+                <jdoc:include type="modules" name="footer" style="<?php echo $this->template . '-default'; ?>" />
             </div>
             <?php endif;?>
         </div>
