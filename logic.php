ce995bd0c0301622de6e272dc269a8d852041088
Uziel Almeida Oliveira <uzielweb@gmail.com>
2023-09-19 19:20:48 -0300

    Update logic.php

--- logic.php
+++ logic.php
@@ -3,7 +3,7 @@
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Filesystem\Folder;
+use Joomla\Filesystem\Folder;
 $app = Factory::getApplication();
 $doc = $app->getDocument();
 // minimize the output html
@@ -31,7 +31,9 @@ $templateParams = $app->getTemplate(true)->params;
 $offcanvasDirection = $templateParams->get('offcanvas_direction', 'start');
 $wa = $doc->getWebAssetManager();
 $war = $wa->getRegistry();
-$logo = $templateParams->get('logo');
+$logo = $templateParams->get('logo', '');
+$logo_alt = $templateParams->get('logo_alt', '') ? $templateParams->get('logo_alt') : $sitename;
+$doc->addFavicon(Uri::root(true) . '/' . $templateParams->get('favicon', ''));
 $doc->setMetaData('viewport', 'width=device-width, initial-scale=1.0');
 $bodyClasses = ($option ? 'option-' . str_replace('com_', '', $option) : 'no-option')
     . ' ' . ($view ? 'view-' . $view : 'no-view')
@@ -48,22 +50,19 @@ $containerFluid = $templateParams->get('container-fluid', 0) ? '-fluid' : '';
 $defaultBoostrapDesktop = '-' . $templateParams->get('default-bootstrap-desktop', 'lg');
 $sidebarWidth = $defaultBoostrapDesktop . '-' . $templateParams->get('sidebar-width', '3');
 // jquery from template or from joomla
-
-if ($templateParams->get('load_jquery_from_template') == 1){
+if ($templateParams->get('load_jquery_from_template') == 1) {
     $wa->registerAndUseScript('jquery_from_template', Uri::root(true) . 'media/templates/site/' . $this->template . '/js/jquery-3.7.0.min.js', array('version' => 'auto'));
     $wa->registerAndUseScript('jquery-noconflict', Uri::root(true) . 'media/templates/site/' . $this->template . '/js/jquery-noconflict.js', array('version' => 'auto'));
     $wa->registerAndUseScript('jquery_migrate_from_template', Uri::root(true) . 'media/templates/site/' . $this->template . '/js/jquery-migrate-3.4.0.min.js', array('version' => 'auto'));
-}
-else{
+} else {
     // load jquery
-HTMLHelper::_('jquery.framework', true, true);
+    HTMLHelper::_('jquery.framework', true, true);
 }
 //  if params is bootstrap from template
-if ($templateParams->get('bootstrap_from_template')){
+if ($templateParams->get('bootstrap_from_template')) {
     $wa->registerAndUseStyle('bootstrap_css', Uri::root(true) . 'media/templates/site/' . $this->template . '/css/bootstrap.min.css', array('version' => 'auto'));
     $wa->registerAndUseScript('bootstrapbundle_js', Uri::root(true) . 'media/templates/site/' . $this->template . '/js/bootstrap.bundle.min.js', array('version' => 'auto'), array('defer' => true));
-}
-else{
+} else {
     // load bootstrap css
     HTMLHelper::_('bootstrap.loadCss', true, $this->direction);
     // load bootstrap js
@@ -71,29 +70,26 @@ else{
 }
 $loadFontAwesome = $templateParams->get('load_fontawesome', 'css_from_joomla');
 //  if params is load fontawesome from template
-if ($loadFontAwesome == 'css_from_template'){
+if ($loadFontAwesome == 'css_from_template') {
     $wa->registerAndUseStyle('fontawesome_css', Uri::root(true) . 'media/templates/site/' . $this->template . '/css/all.min.css', array('version' => 'auto'));
-    
-}
-elseif ($loadFontAwesome == 'js_from_template'){
-      $wa->registerAndUseScript('fontawesome_js', Uri::root(true) . 'media/templates/site/' . $this->template . '/js/all.min.js', array('version' => 'auto'), array('defer' => true));
-}
-elseif ($loadFontAwesome == 'css_from_joomla'){
+} elseif ($loadFontAwesome == 'js_from_template') {
+    $wa->registerAndUseScript('fontawesome_js', Uri::root(true) . 'media/templates/site/' . $this->template . '/js/all.min.js', array('version' => 'auto'), array('defer' => true));
+} elseif ($loadFontAwesome == 'css_from_joomla') {
     // load fontawesome
     $wa->registerAndUseStyle('fontawesome', 'media/vendor/fontawesome-free/css/all.min.css', array('version' => 'auto'));
-   
-}
-else{
+} else {
     // nothing
-}
-//  scan template css folder and load all css files with "custom" in the name
-foreach (Folder::files(JPATH_ROOT.'/media/templates/site/' . $this->template . '/css', 'custom', true, true) as $i => $file) {
-    $wa->registerAndUseStyle(pathinfo($file, PATHINFO_FILENAME),  Uri::root(true) . 'media/templates/site/' . $this->template . '/css' . '/' . basename($file), array('version' => 'auto'));
 }
 // load Joomla 4 system icons
 $wa->registerAndUseStyle('icons', 'media/system/css/joomla-fontawesome.min.css', array('version' => 'auto'));
 $wa->registerAndUseStyle('template-css', Uri::root(true) . 'media/templates/site/' . $this->template . '/css/template.css', array('version' => 'auto'));
-
 //  load template js only after jquery and bootstrap
 $wa->registerAndUseScript('template-js', Uri::root(true) . 'media/templates/site/' . $this->template . '/js/template.js', array('version' => 'auto'), array('defer' => true));
-
+//  scan template css folder and load all css files with "customfont" in the name
+foreach (Folder::files(JPATH_ROOT . '/media/templates/site/' . $this->template . '/css', 'customfont', true, true) as $i => $file) {
+    $wa->registerAndUseStyle(pathinfo($file, PATHINFO_FILENAME), Uri::root(true) . 'media/templates/site/' . $this->template . '/css' . '/' . basename($file), array('version' => 'auto'));
+}
+//  scan template css folder and load all css files with "custom" in the name
+foreach (Folder::files(JPATH_ROOT . '/media/templates/site/' . $this->template . '/css', 'custom', true, true) as $i => $file) {
+    $wa->registerAndUseStyle(pathinfo($file, PATHINFO_FILENAME), Uri::root(true) . 'media/templates/site/' . $this->template . '/css' . '/' . basename($file), array('version' => 'auto'));
+}
