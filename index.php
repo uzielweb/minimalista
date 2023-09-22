bb31b10f3e3f0d616cbcd59e6d772acf4ac7824b
Uziel Almeida Oliveira <uzielweb@gmail.com>
2023-09-22 02:36:57 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Templates.minmalista
+ *
+ * @copyright   (C) 2017 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
 defined('_JEXEC') or die;
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
+
+/** @var Joomla\CMS\Document\HtmlDocument $this */
+
 include_once JPATH_THEMES . '/minimalista/logic.php';
 ?>
 <!doctype html>
