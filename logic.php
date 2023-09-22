53bf1b9cd48b0f4c68a2ad6fbf7ba5fbd47fa43c
Uziel Almeida Oliveira <uzielweb@gmail.com>
2023-09-22 02:39:34 -0300

    Update logic.php

--- logic.php
+++ logic.php
@@ -1,9 +1,24 @@
 <?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  Templates.minmalista
+ *
+ * @copyright   (C) 2017 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
 // Include common libraries and dependencies
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Uri\Uri;
 use Joomla\CMS\Filesystem\Folder;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Uri\Uri;
+
+/** @var Joomla\CMS\Document\HtmlDocument $this */
+
 
 // Database connection setup
 $db = Factory::getContainer()->get('DatabaseDriver');
