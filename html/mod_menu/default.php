a05b4db0b0e2fbde6d60e51bb8a5716b8a118c9a
Uziel Almeida Oliveira <uzielweb@gmail.com>
2023-11-14 08:11:52 -0300

    Add files via upload

--- html/mod_menu/default.php
+++ html/mod_menu/default.php
@@ -25,7 +25,7 @@ if ($tagId = $params->get('tag_id', '')) {
 
 // The menu class is deprecated. Use mod-menu instead
 ?>
-<ul<?php echo $id; ?> class="mod-menu mod-list nav <?php echo $class_sfx; ?>">
+<ul<?php echo $id; ?> class="mod-menu mod-list navbar-nav <?php echo $class_sfx; ?>">
 <?php foreach ($list as $i => &$item) {
     $itemParams = $item->getParams();
     $class      = 'nav-item item-' . $item->id;
