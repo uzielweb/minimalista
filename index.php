fa42c52f37f46f4dc226aa057293c1eef3969397
Uziel Almeida Oliveira <uzielweb@gmail.com>
2024-01-30 08:34:11 -0300

    Update index.php

--- index.php
+++ index.php
@@ -52,8 +52,7 @@ include_once JPATH_THEMES . '/minimalista/logic.php';
                                 aria-label="Close"></button>
                         </div>
                         <div class="offcanvas-body">
-                            <jdoc:include type="modules" name="menu"
-                                style="<?php echo $this->template . '-default'; ?>" />
+                            <jdoc:include type="modules" name="menu" style="<?php echo $this->template . '-default'; ?>" />
                         </div>
                     </div>
                 </div>
@@ -83,48 +82,42 @@ if ($sectionsBeforeComponent) {
                 <div class="inner">
                     <?php if ($this->countModules('main-top')): ?>
                     <div class="main-top row">
-                        <jdoc:include type="modules" name="main-top"
-                            style="<?php echo $this->template . '-default'; ?>" />
+                        <jdoc:include type="modules" name="main-top" style="<?php echo $this->template . '-default'; ?>" />
                     </div>
                     <?php endif;?>
                     <div class="row">
                         <?php if ($this->countModules('sidebar-left')): ?>
                         <div class="sidebar-left col-12 col<?php echo $sidebarWidth; ?>">
                             <div class="row">
-                                <jdoc:include type="modules" name="sidebar-left"
-                                    style="<?php echo $this->template . '-default'; ?>" />
+                                <jdoc:include type="modules" name="sidebar-left" style="<?php echo $this->template . '-default'; ?>" />
                             </div>
                         </div>
                         <?php endif;?>
                         <div class="component col-12 col<?php echo $mainWidth; ?>">
                             <?php if ($this->countModules('content-top')): ?>
                             <div class="row">
-                                <jdoc:include type="modules" name="content-top"
-                                    style="<?php echo $this->template . '-default'; ?>" />
+                                <jdoc:include type="modules" name="content-top" style="<?php echo $this->template . '-default'; ?>" />
                             </div>
                             <?php endif;?>
                             <jdoc:include type="message" />
                             <jdoc:include type="component" />
                             <?php if ($this->countModules('content-bottom')): ?>
                             <div class="row">
-                                <jdoc:include type="modules" name="content-bottom"
-                                    style="<?php echo $this->template . '-default'; ?>" />
+                                <jdoc:include type="modules" name="content-bottom" style="<?php echo $this->template . '-default'; ?>" />
                             </div>
                             <?php endif;?>
                         </div>
                         <?php if ($this->countModules('sidebar-right')): ?>
                         <div class="sidebar-right col-12 col<?php echo $sidebarWidth; ?>">
                             <div class="row">
-                                <jdoc:include type="modules" name="sidebar-right"
-                                    style="<?php echo $this->template . '-default'; ?>" />
+                                <jdoc:include type="modules" name="sidebar-right" style="<?php echo $this->template . '-default'; ?>" />
                             </div>
                         </div>
                         <?php endif;?>
                     </div>
                     <?php if ($this->countModules('main-bottom')): ?>
                     <div class="main-bottom row">
-                        <jdoc:include type="modules" name="main-bottom"
-                            style="<?php echo $this->template . '-default'; ?>" />
+                        <jdoc:include type="modules" name="main-bottom" style="<?php echo $this->template . '-default'; ?>" />
                     </div>
                     <?php endif;?>
                 </div>
