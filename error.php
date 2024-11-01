06531548358047fe1ae5e1b0544a83a8bebadfaa
Uziel Almeida Oliveira <uzielweb@gmail.com>
2024-11-01 01:23:04 -0300

    Update error.php

--- error.php
+++ error.php
@@ -7,6 +7,7 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('_JEXEC') or die;
+use Joomla\CMS\Factory;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Uri\Uri;
 use JOomla\CMS\Router\Route;
