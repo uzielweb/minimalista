4cbc9dad1450a23d485781ec0d2441d325e191db
Uziel Almeida Oliveira <uzielweb@gmail.com>
2025-01-09 19:02:18 -0300

    Update component.php

--- component.php
+++ component.php
@@ -1,5 +1,7 @@
 <?php
-@license     GNU General Public License version 2 or later; see LICENSE.txt
+/**
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+*/
 defined('_JEXEC') or die;
 use Joomla\CMS\Document\Document;
 use Joomla\CMS\Factory;
