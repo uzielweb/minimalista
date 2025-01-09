0cbc5cbbc80272a67532280941302e800afc154c
Uziel Almeida Oliveira <uzielweb@gmail.com>
2025-01-09 19:02:49 -0300

    Update component.php

--- component.php
+++ component.php
@@ -1,7 +1,11 @@
 <?php
 /**
+ * @package     Joomla.Site
+ * @subpackage  Templates.minmalista
+ *
+ * @copyright   (C) 2017 Open Source Matters, Inc. <https://www.joomla.org>
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
-*/
+ */
 defined('_JEXEC') or die;
 use Joomla\CMS\Document\Document;
 use Joomla\CMS\Factory;
