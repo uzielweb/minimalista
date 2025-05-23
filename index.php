42ae6c17aed52441ab9e7781dfb0e89a2df97d5b
Uziel Almeida Oliveira <uzielweb@gmail.com>
2025-05-23 07:43:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -13,11 +13,13 @@ include_once JPATH_THEMES . '/minimalista/logic.php';
 ?>
 <!doctype html>
 <html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+
 <head>
     <jdoc:include type="metas" />
     <jdoc:include type="styles" />
     <jdoc:include type="scripts" />
 </head>
+
 <body class="<?php echo $bodyClasses; ?>">
     <?php echo $startBodyCode; ?>
     <?php
@@ -33,25 +35,26 @@ if ($sectionsBeforeHeader) {
     <?php if (($logo && $logo_position == 'header-navbar') || $this->countModules('header') || $this->countModules('menu') || $this->countModules('header-top') || $this->countModules('header-bottom')): ?>
     <header class="header" id="header">
         <?php if ($this->countModules('header-top')): ?>
-            <div class="header-top" id="header-top">
-                <div class="container<?php echo $containerFluid; ?>">
-                    <div class="row">
-                        <jdoc:include type="modules" name="header-top" style="<?php echo $templateOriginal . '-default'; ?>" />
-                    </div>
+        <div class="header-top" id="header-top">
+            <div class="container<?php echo $containerFluid; ?>">
+                <div class="row">
+                    <jdoc:include type="modules" name="header-top" style="<?php echo $templateOriginal . '-default'; ?>" />
                 </div>
             </div>
+        </div>
         <?php endif;?>
         <?php if ($this->countModules('header')): ?>
-            <div class="main-header" id="main-header">
-                <div class="container<?php echo $containerFluid; ?>">
-                    <div class="row">
-                        <jdoc:include type="modules" name="header" style="<?php echo $templateOriginal . '-default'; ?>" />
-                    </div>
+        <div class="main-header" id="main-header">
+            <div class="container<?php echo $containerFluid; ?>">
+                <div class="row">
+                    <jdoc:include type="modules" name="header" style="<?php echo $templateOriginal . '-default'; ?>" />
                 </div>
             </div>
+        </div>
         <?php endif;?>
         <div class="container<?php echo $containerFluid; ?>">
-            <!-- navbar offcanvas bootstrap 5 -->            <nav class="navbar navbar-expand<?php echo $defaultBoostrapDesktop; ?> row">
+            <!-- navbar offcanvas bootstrap 5 -->
+            <nav class="navbar navbar-expand<?php echo $defaultBoostrapDesktop; ?> row">
                 <div class="container-fluid">
                     <?php if ($logo && $logo_position == 'header-navbar'): ?>
                     <a class="navbar-brand" href="<?php echo $this->baseurl; ?>">
@@ -59,18 +62,18 @@ if ($sectionsBeforeHeader) {
                     </a>
                     <?php endif;?>
                     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
-                        data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                            data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
                         <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="offcanvas offcanvas-<?php echo $offcanvasDirection; ?>" tabindex="-1"
-                        id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                         id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                         <div class="offcanvas-header">
                             <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
-                                aria-label="Close"></button>
+                                    aria-label="Close"></button>
                         </div>
                         <div class="offcanvas-body">
                             <div class="row w-100">
-                            <jdoc:include type="modules" name="menu" style="<?php echo $templateOriginal . '-default'; ?>" />
+                                <jdoc:include type="modules" name="menu" style="<?php echo $templateOriginal . '-default'; ?>" />
                             </div>
                         </div>
                     </div>
@@ -85,13 +88,13 @@ if ($sectionsBeforeHeader) {
             </nav>
         </div>
         <?php if ($this->countModules('header-bottom')): ?>
-            <div class="header-bottom" id="header-bottom">
-                <div class="container<?php echo $containerFluid; ?>">
-                    <div class="row">
-                        <jdoc:include type="modules" name="header-bottom" style="<?php echo $templateOriginal . '-default'; ?>" />
-                    </div>
+        <div class="header-bottom" id="header-bottom">
+            <div class="container<?php echo $containerFluid; ?>">
+                <div class="row">
+                    <jdoc:include type="modules" name="header-bottom" style="<?php echo $templateOriginal . '-default'; ?>" />
                 </div>
             </div>
+        </div>
         <?php endif;?>
     </header>
     <?php endif;?>
@@ -135,7 +138,7 @@ if ($sectionsBeforeComponent) {
         <section id="section-component" class="section-component">
             <div class="container<?php echo $containerFluid; ?>">
                 <div class="inner">
-                <?php if ($this->countModules('breadcrumbs')): ?>
+                    <?php if ($this->countModules('breadcrumbs')): ?>
                     <div class="component-breadcrumbs">
                         <div class="row">
                             <jdoc:include type="modules" name="breadcrumbs" style="<?php echo $templateOriginal . '-default'; ?>" />
@@ -149,7 +152,7 @@ if ($sectionsBeforeComponent) {
                         </div>
                     </div>
                     <?php endif;?>
-                    <div class="row">                        <?php if ($this->countModules('sidebar-left') || ($logo && $logo_position == 'sidebar-left')): ?>
+                    <div class="row"> <?php if ($this->countModules('sidebar-left') || ($logo && $logo_position == 'sidebar-left') && $this->countModules('sidebar-left')): ?>
                         <div class="sidebar-left col-12 col<?php echo $sidebarWidth; ?>" id="sidebar-left">
                             <?php if ($logo && $logo_position == 'sidebar-left' && $this->countModules('sidebar-left')): ?>
                             <div class="sidebar-logo">
@@ -158,7 +161,7 @@ if ($sectionsBeforeComponent) {
                                 </a>
                             </div>
                             <?php endif; ?>
-                              <?php if ($this->countModules('sidebar-left')): ?>
+                            <?php if ($this->countModules('sidebar-left')): ?>
                             <div class="row">
                                 <jdoc:include type="modules" name="sidebar-left" style="<?php echo $templateOriginal . '-default'; ?>" />
                             </div>
@@ -167,22 +170,22 @@ if ($sectionsBeforeComponent) {
                         <?php endif;?>
                         <div class="component col-12 col<?php echo $mainWidth; ?>">
                             <?php if ($this->countModules('content-top')): ?>
-                                <div class="content-top" id="content-top">
-                                    <div class="row">
-                                        <jdoc:include type="modules" name="content-top" style="<?php echo $templateOriginal . '-default'; ?>" />
-                                    </div>
+                            <div class="content-top" id="content-top">
+                                <div class="row">
+                                    <jdoc:include type="modules" name="content-top" style="<?php echo $templateOriginal . '-default'; ?>" />
                                 </div>
+                            </div>
                             <?php endif;?>
                             <jdoc:include type="message" />
                             <jdoc:include type="component" />
                             <?php if ($this->countModules('content-bottom')): ?>
-                                <div class="content-bottom" id="content-bottom">
-                                    <div class="row">
-                                        <jdoc:include type="modules" name="content-bottom" style="<?php echo $templateOriginal . '-default'; ?>" />
-                                    </div>
+                            <div class="content-bottom" id="content-bottom">
+                                <div class="row">
+                                    <jdoc:include type="modules" name="content-bottom" style="<?php echo $templateOriginal . '-default'; ?>" />
                                 </div>
+                            </div>
                             <?php endif;?>
-                        </div>                        <?php if ($this->countModules('sidebar-right') || ($logo && $logo_position == 'sidebar-right')): ?>
+                        </div> <?php if ($this->countModules('sidebar-right') || ($logo && $logo_position == 'sidebar-right') && $this->countModules('sidebar-right')): ?>
                         <div class="sidebar-right col-12 col<?php echo $sidebarWidth; ?>" id="sidebar-right">
                             <?php if ($logo && $logo_position == 'sidebar-right' && $this->countModules('sidebar-right')): ?>
                             <div class="sidebar-logo">
@@ -191,7 +194,7 @@ if ($sectionsBeforeComponent) {
                                 </a>
                             </div>
                             <?php endif; ?>
-                              <?php if ($this->countModules('sidebar-right')): ?>
+                            <?php if ($this->countModules('sidebar-right')): ?>
                             <div class="row">
                                 <jdoc:include type="modules" name="sidebar-right" style="<?php echo $templateOriginal . '-default'; ?>" />
                             </div>
@@ -240,10 +243,11 @@ if ($sectionsAfterComponent) {
     <?php echo $endBodyCode; ?>
     <?php if ($backtotop): ?>
     <button href="#top" id="back-top" class="btn back-to-top-link"
-        aria-label="<?php echo Text::_('TPL_MINIMALISTA_BACKTOTOP'); ?>">
+            aria-label="<?php echo Text::_('TPL_MINIMALISTA_BACKTOTOP'); ?>">
         <i class="fas fa-arrow-up" aria-hidden="true"></i>
     </button>
     <?php endif;?>
     <jdoc:include type="modules" name="debug" style="none" />
 </body>
+
 </html>
